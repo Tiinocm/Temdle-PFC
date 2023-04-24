3e0245c74c692cf9a0747fac3b5281a6fba2d2f9
Tiinocm <tinoclase@gmail.com>
2023-04-24 17:53:33 +0200

    different tests

--- Temdle-angular/src/app/win-modal/win-modal.component.ts
+++ Temdle-angular/src/app/win-modal/win-modal.component.ts
@@ -14,19 +14,38 @@ export class WinModalComponent {
   constructor (@Inject(MAT_DIALOG_DATA) public data : any) {}
 
   public temInfo : any[] = []
+  public printInfo : any[] = []
+  public targetInfo : any;
+  public summary : string = "";
 
   ngOnInit()
   {
     let arrayTems : number[] = this.data.tems;
 
     let target = arrayTems.pop()
+
+    this.targetInfo = this.data.info[target!];
     for (let i = 0; i < this.data.info.length; i++) {
       if (arrayTems.indexOf(this.data.info[i].number) !== -1) {
         this.temInfo.push(this.data.info[i])
       }
     }
+
     
-    
+/*     let targetHeight = this.targetInfo!.details.height.cm
+    let targetWeight = this.targetInfo!.details.weight.kg
+
+    for (let i = 0; i < this.temInfo.length; i++) {
+      let height = this.temInfo[i].details.height.cm;
+
+      if (height < targetHeight) {
+        this.summary += "&#128316;"
+      }else if(height > targetHeight){
+        this.summary += "&#128317;"
+      }else{
+        this.summary += "&#128316;"
+      }
+    } */
     
     
   }
